72f2cf303540942a0404c8f9b4096a82f6f4c136
Joey <headmastereggy@gmail.com>
2023-08-18 17:45:24 +0800

    changes

--- src/main/java/com/example/ArrayListDemo1.java
+++ src/main/java/com/example/ArrayListDemo1.java
@@ -1,8 +1,11 @@
 package com.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+
 public class ArrayListDemo1 {
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        System.out.println(list.size());
     }
 }
